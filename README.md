620042ea7abfb028d3f1183c48bb08b671e92faf
Kevin Piacentini <kevin@Mac-Pro-de-Kevin.local>
2016-05-29 11:45:00 +0200

    Add quickstart structure

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # angular2-tour-of-heroes
+
+Following the tutorial from official angular documentation available here : https://angular.io/docs/ts/latest/tutorial/
+
---
+++ app/app.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'my-app',
+	template: '<h1>Welcome to my quickstart angular 2 app!</h1>'
+})
+export class AppComponent { }
